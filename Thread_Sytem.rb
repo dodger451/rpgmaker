e6c2df468d55f04e951a43d297002f43e10a4c64
david <david-github@latotzky.de>
2015-08-12 22:55:57 +0200

    cleanup, dbg

--- Thread_Sytem.rb
+++ Thread_Sytem.rb
@@ -1,6 +1,59 @@
+﻿#==============================================================================
+# 
+# ▼ dodger451 rpgmaker - Threat System v0.1
+# 
+#==============================================================================
+
+$imported = {} if $imported.nil?
+$imported["dodger451-ThreatSystem"] = false
+
+#==============================================================================
+# ▼ Introduction
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# This is a script to improve the AI of Enemies for RPG Maker VX Ace. During a 
+# battle, the script lets enemies choose targets based on the accumulated 
+# "threat" they feel for each opponent (multiplied by his tgr). This script 
+# will maintain a threat table for each enemy, threat rises when an party member
+# hurts an enemy or when he heals another member.
+# 
+#==============================================================================
+# ▼ Instructions
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# To install this script, open up your script editor and copy/paste this script
+# to an open slot below ▼ Materials/素材 but above ▼ Main. Remember to save.
+# 
+# Adjust the module's settings to adjust the behaviour.
+# 
+#==============================================================================
+# ▼ Compatibility
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# This script is made strictly for RPG Maker VX Ace. It is highly unlikely that
+# it will run with RPG Maker VX without adjusting.
+# 
+#==============================================================================
+
+module DODGER451
+  module THREAT
+    
+    #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    # - General Threat Settings -
+    #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    # In this section, you can adjust the threat settings for your game
+    # such as the modifiers and constants for threat manipulation
+    #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    DEBUG_ON     = true   
+
+  end # THREAT
+end # DODGER451
 
 
 class Game_Threat
+
+  def dbg(str)
+    if(DODGER451::THREAT::DEBUG_ON)
+      p str
+    end
+  end
 
   #global list
   def threat_tables_initialized?;
@@ -50,20 +103,17 @@ class Game_Threat
   #modify threat
   
   def add_threat(enemy_defender, actor_attacker, additional_threat)
-    p "adding threat."
-    init_threat(enemy_defender.name, actor_attacker.name) unless threat_initialized?(enemy_defender.name, actor_attacker.name);
-      
+    init_threat(enemy_defender.name, actor_attacker.name) unless threat_initialized?(enemy_defender.name, actor_attacker.name);      
     $threat_tables[enemy_defender.name][actor_attacker.name] +=  additional_threat;
   end
+
   # updates the threat an enemy (and his teammates) feel from a actor 
   # after the actor caused hp- or mp-damage.
   # Called after an actor attacked an enemy (execute_damage in Game_Battler)
   # result, battler, battler -> self
   def update_threat_on_damage(result, actor_attacker, enemy_defender)
-    
-    # todo handle mp,..
     # todo emit also to teammates
-    # todo smsrter scaling, e.g. relative to hp, lg scaled,...
+    # todo smarter scaling, e.g. relative to hp, lg scaled,...
     additional_threat = 1 + result.hp_damage
     p actor_attacker.name + " causes " + result.hp_damage.to_s + " damage on " + enemy_defender.name+", " +enemy_defender.name+" feels more threatened (+"+additional_threat.to_s+")"
     add_threat(enemy_defender, actor_attacker, additional_threat)
@@ -82,9 +132,9 @@ class Game_Threat
     return self unless alive_count>0
  
     additional_threat = ((1 + (-result.hp_damage)).to_f/alive_count.to_f).to_i     
-    p actor_healer.name + " heals " + result.hp_damage.to_s + " hp on " + actor_healed.name+", all his enemies feel more threatened (+"+additional_threat.to_s+" each)"
+    dbg actor_healer.name + " heals " + result.hp_damage.to_s + " hp on " + actor_healed.name+", all his enemies feel more threatened (+"+additional_threat.to_s+" each)"
     actor_healed.opponents_unit.alive_members.each do |enemy|
-      p " -> " + enemy.name + " feels " + additional_threat.to_s + " more threatended by  " + actor_healed.name
+      dbg " -> " + enemy.name + " feels " + additional_threat.to_s + " more threatended by  " + actor_healer.name
       add_threat(enemy, actor_healer, additional_threat)
     end
 
@@ -110,16 +160,21 @@ class Game_Threat
   # * Find weighted random opponent for attacker based on threat and tgr
   #--------------------------------------------------------------------------
   def random_target(attacker)
+    dbg attacker.name + " finds thread-based target"
+    attacker.opponents_unit.alive_members.each do |enemy|
+      dbg attacker.name + " feels threaten by " + enemy.name + " (tgr="+enemy.tgr.to_s+", threat="+$game_threat.get_threat(attacker.name, enemy.name).to_s+"): " + $game_threat.tgr_threat(attacker.name, enemy).to_s
+    end
+
     tgr_rand = rand * tgr_threat_sum(attacker.name, attacker.opponents_unit)
     attacker.opponents_unit.alive_members.each do |member|
       tgr_rand -= tgr_threat(attacker.name, member)
       #return member if tgr_rand < 0
       if tgr_rand < 0
-        p " choose " + member.name + "(tgr_threat: " + tgr_threat(attacker.name, member).to_s + ")"
+        dbg " choose " + member.name + "(" + tgr_threat(attacker.name, member).to_s + ")"
         return member 
       end
     end
-    p " choose as fallback " + member.name 
+    dbg " choose as fallback " + member.name 
     attacker.opponents_unit.alive_members[0]
   end 
   #--------------------------------------------------------------------------
@@ -154,14 +209,11 @@ class Game_Battler < Game_BattlerBase
 
   def update_threat_on_execute_damage(user)
     
-    p ""+user.name+" cause " +@result.hp_damage.to_s+" damage on "+self.name
-
     if (self.is_a?(Game_Enemy) && user.is_a?(Game_Actor))
       $game_threat.update_threat_on_damage(@result, user, self)
     end
     if (self.is_a?(Game_Actor) && user.is_a?(Game_Actor) && @result.hp_damage < 0)
       $game_threat.update_threat_on_heal(@result, user, self)
-      #p "actor receives healing from actor, all enemy should  gain threat on user"
     end
   end
 end # Game_Battler
@@ -171,10 +223,6 @@ class Game_Action
   def targets_for_opponents
     if (!item.for_random? && item.for_one? && (@target_index < 0))
       
-      p subject.name + " finds thread-based target for action '" + item.name + "'"
-      subject.opponents_unit.alive_members.each do |enemy|
-        p subject.name + " feels threaten by " + enemy.name + " (tgr="+enemy.tgr.to_s+"): " + $game_threat.tgr_threat(subject.name, enemy).to_s
-      end
       
       num = 1 + (attack? ? subject.atk_times_add.to_i : 0)
       return [$game_threat.random_target(subject)] * num
